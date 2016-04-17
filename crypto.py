ac3059ab6165e041e0440097ebd68a339a2b63a7
Денис Шайхисламов <sdenis1995@gmail.com>
2016-04-17 17:59:38 +0300

    Added new option

--- crypto.py
+++ crypto.py
@@ -15,14 +15,16 @@ parser.add_argument('--secret_key', help='set name of file containing secret key
 #parser.add_argument('--parameters', help='set name of file containing parameters for DigitalSignatureParameters class (has default value, is used in KEK generation, public key generation)')
 parser.add_argument('--IV', help='set name of file containing initializing vector for GOST 28147-89 Imit or encrypt/decrypt in feedback mode (has default value)')
 parser.add_argument('--filename', help='set name of file containing message to encrypt/decrypt or hash')
-parser.add_argument('--public_key', help='set name of file containing public key for generating KEK')
-parser.add_argument('--CEK', help='set name of file containing CEK for key wrapping')
+parser.add_argument('--public_key', help='set name of file containing public key for generating KEK and complex encryption/decryption')
+parser.add_argument('--CEK', help='set name of file containing CEK for key wrapping and complex encryption. Also used to save CEK key if --complex_decrypt is used')
 parser.add_argument('--hex', action="store_true", help='specify if message in filename is in hex')
 group.add_argument('--hash', action="store_true", help='GOST R 34.11-2012 hash')
 group.add_argument('--generate_public_key', action="store_true", help='Generates public key based on secret key')
 group.add_argument('--generate_KEK', action="store_true", help='Generates Key Encryption Key based on secret key and public key (specify secret key by using --secret_key and public key by using --public_key)')
 group.add_argument('--encrypt_fm', action="store_true", help='Encrypts message according to GOST 28147-89 (feedback mode)')
 group.add_argument('--decrypt_fm', action="store_true", help='Decrypts message according to GOST 28147-89 (feedback mode). Use --hex when using --filename')
+group.add_argument('--complex_encrypt', action="store_true", help='Full cycle of encryption, generates KEK, encrypts message using CEK key, wraps the CEK by KEK. use --filename to specify message to encrypt, --public_key and --secret_key for generating KEK')
+group.add_argument('--complex_decrypt', action="store_true", help='Full cycle of decryption, generates KEK, unwraps wrapped CEK and decrypts encrypted message. use --filename to specify encrypted message with wrapped CEK, --public_key and --secret_key for generating KEK')
 group.add_argument('--ECB_encrypt', action="store_true", help='Encrypts message according to GOST 28147-89 (ECB mode)')
 group.add_argument('--ECB_decrypt', action="store_true", help='Decrypts message according to GOST 28147-89 (ECB mode). Use --hex when using --filename')
 group.add_argument('--imit', action="store_true", help='32-bit result of the GOST 28147-89 in MAC mode')
@@ -56,12 +58,13 @@ if args.filename:
     msg = fp.read().encode()
   fp.close()
 if args.CEK:
-  fp = open(args.CEK)
-  if not fp:
-    print("Can't open file specified in --CEK")
-    sys.exit(1)
-  CEK = int(fp.readline(), 16)
-  fp.close()
+  if not args.complex_decrypt:
+    fp = open(args.CEK)
+    if not fp:
+      print("Can't open file specified in --CEK")
+      sys.exit(1)
+    CEK = int(fp.readline(), 16)
+    fp.close()
   
 if args.public_key:
   fp = open(args.public_key)
@@ -132,4 +135,34 @@ if args.key_unwrap:
     output_file.write(hex(to_int(CEK)))
   else:
     print("Cant unwrap, data is corrupted")
+
+if args.complex_encrypt:
+  KEK = generate_common_KEK(secret_key, public_key, base_parameters_1)
+  print(to_int(KEK))
+  wrapped = GOST2814789KeyWrap(CEK.to_bytes(32, 'big'), KEK)
+  cipher = cipher_feedback_mode_encode(msg, CEK.to_bytes(32, 'big'), wrapped[0:8])
+  output_file.write(hex(to_int(cipher)) + "\n")
+  output_file.write(hex(to_int(wrapped)))
+  
+if args.complex_decrypt:
+  fp = open(args.filename)
+  if not fp:
+    print("Can't open file specified in --filename")
+    sys.exit(1)
+  KEK = generate_common_KEK(secret_key, public_key, base_parameters_1)
+  msg = int(fp.readline(), 16)
+  print(to_int(KEK))
+  msg = msg.to_bytes((msg.bit_length() // 8) + 1 , 'big')
+  wrap = int(fp.readline(), 16).to_bytes(44, 'big')
+  
+  CEK = GOST2814789KeyUnWrap(wrap, KEK)
+  if CEK:
+    cek_file = open(args.CEK, "w")
+    cek_file.write(hex(to_int(CEK)))
+  else:
+    print("Cant unwrap, data is corrupted")
+    sys.exit(1)
+  decipher = cipher_feedback_mode_decode(msg, CEK, wrap[0 : 8])
+  output_file.write(decipher.decode())
+  
 output_file.close()
